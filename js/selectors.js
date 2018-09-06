d2f59d57f49bb1da05cc088e69b22b871f26b455
Annie Eby <eby.annie@gmail.com>
2018-09-06 14:33:36 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,18 @@
 // declare your functions here...
+function paragraphSelector() {
+  $('p-tag')
+}
+
+function lastImageSelector() {
+  $('#baby-ninja')
+}
+
+function divSelector() {
+  $('.pics')
+}
+
+function firstListItem() {
+  $('#pic-list ul').first() 
+}
+// "firstListItem() should return the first list item in the unordered list"
+//https://api.jquery.com/first/
